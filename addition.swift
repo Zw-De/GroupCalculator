dc951dcfc57191b7ceeb6421236266f214078f29
APCSP <apcsp@Cart-Mac-1.local>
2025-09-10 15:19:46 -0700

    addition code by jumaa

---
+++ addition.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+print("Enter the first number:")
+let firstNumber = Int(readline()!)!
+
+print("Enter the second number:")
+let secondNumber = Int(readline()!)!
+
+let sum = firstNumber + secondNumber
+
+print("The sum is \(sum)")
