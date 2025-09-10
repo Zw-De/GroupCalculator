21b5924f50dc9c23a78bece7675be824ab323ffc
APCSP <apcsp@Cart-Mac-1.local>
2025-09-10 11:06:40 -0700

    Added Menu to our calculator

---
+++ calculator.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+// Menu function to display options to the user
+func showMenu() {
+print("""
+select an operation:
+1: Add
+2: Substract
+3: Multiply
+4: Divide
+""")
+}
+
+showMenu() //display menu
