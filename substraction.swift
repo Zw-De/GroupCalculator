e3dc225af914d786add2ad008e24fdcf51dfedfb
APCSP <apcsp@Cart-Mac-1.local>
2025-09-10 15:21:20 -0700

    substraction code by jumaa

---
+++ substraction.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+print("Enter the first number:")
+let firstNumber = Int(readLine()!)!
+
+
+print("Enter the second number:")
+let secondNumber = Int(readLine()!)!
+
+let difference = firstNumber - secondNumber
+
+print("The difference is \(difference)")
