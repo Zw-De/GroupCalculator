ce9bee88335c072548d59d9bcd6d4b071f11d580
APCSP <apcsp@Cart-Mac-1.local>
2025-09-24 15:41:53 -0700

    Jumaa added new changes

--- calculator.swift
+++ calculator.swift
@@ -1,14 +1,40 @@
 import Foundation
 
-// Menu function to display options to the user
 func showMenu() {
 print("""
 select an operation:
 1: Add
-2: Substract
-3: Multiply
-4: Divide
+2: Subtract
 """)
 }
 
-showMenu() //display menu
+showMenu() 
+
+guard let operation = readLine() else {
+print("Invalid input. Please enter 1 or 2.")
+exit(0)
+}
+
+if operation == "1" {
+print("Enter the first number:")
+let firstNumber = Int(readLine()!)!
+
+print("Enter the second number:")
+let secondNumber = Int(readLine()!)!
+
+let sum = firstNumber + secondNumber
+print("The sum is \(sum)")
+
+} else if operation == "2" {
+print("Enter the first number:")
+let firstNumber = Int(readLine()!)!
+
+print("Enter the second number:")
+let secondNumber = Int(readLine()!)!
+
+let difference = firstNumber - secondNumber
+print("The difference is \(difference)")
+
+} else {
+print("Invalid input. Please enter 1 or 2.")
+}
